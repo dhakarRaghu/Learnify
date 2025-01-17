2937eb4bfa58ba34f1162ffa2f713b967ed70bce
dhakarRaghu <rdhakad2002@gmail.com>
2025-01-18 00:32:33 +0530

    added the clerk authentication and landing page

---
+++ learnify/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { User } from '@clerk/nextjs/server'
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>
+      user dashboard
+   </div>
+  )
+}
+
+export default dashboard
