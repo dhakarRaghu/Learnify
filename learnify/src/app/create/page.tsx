d07561453dadb9eda2a0090dd57bdf0d66aba174
dhakarRaghu <rdhakad2002@gmail.com>
2025-01-20 01:56:11 +0530

    add YouTube api for getting video and video transcript  and add chapter management features

--- learnify/src/app/create/page.tsx
+++ learnify/src/app/create/page.tsx
@@ -12,7 +12,7 @@ const CreatePage = async (props: Props) => {
   if (!session?.user) {
     return redirect("/gallery");
   }
-  // const isPro = await checkSubscription();
+  const isPro = true;
   return (
     <div className="flex flex-col items-start max-w-xl px-8 mx-auto my-16 sm:px-0">
       <h1 className="self-center text-3xl font-bold text-center sm:text-6xl">
@@ -21,13 +21,13 @@ const CreatePage = async (props: Props) => {
       <div className="flex p-4 mt-5 border-none bg-secondary">
         <InfoIcon className="w-12 h-12 mr-3 text-blue-400" />
         <div>
-          Enter a course title, or what you want to learn about. Then enter a
+          Enter in a course title, or what you want to learn about. Then enter a
           list of units, which are the specifics you want to learn. And our AI
           will generate a course for you!
         </div>
       </div>
 
-      <CreateCourseForm/>
+      <CreateCourseForm isPro={isPro} />
     </div>
   );
 };
